12a315221b479fcea29b5117ab45b7aafbf1169f
Kohei Sato <kohei_sato@crosshead.co.jp>
2023-04-03 15:06:59 +0900

    feat: Kintone Date DateTime form feat

--- src/settingScreen/components/KintoneDateTime.tsx
+++ src/settingScreen/components/KintoneDateTime.tsx
@@ -5,25 +5,31 @@ import { useSkipEffect } from "../utils";
 /**
  * @param onChange - Event handler fired when the entered value changes
  */
-export default (props: { 
+export default (props: {
+  value: string
   onChange?: (value: string) => void }
   ) => {
-  const [date, setDate] = useState('');
-  const [time, setTime] = useState('');
+  const [date, setDate] = useState(props.value.split('T')[0]);
+  const [time, setTime] = useState(props.value.split('T')[1]);
 
   useSkipEffect(() => {
     props.onChange && props?.onChange(`${date}T${time}:00`);
   },
   [date, time]);
 
+  useSkipEffect(() => {
+    setDate(props.value.split('T')[0]);
+    setTime(props.value.split('T')[1]);
+  },[props.value]);
+
   return (
-    <>
+    <div>
       <input type="date" css={globalStyle.inputDateOrTime} value={date} onChange={(event) => {
         setDate(event.target.value);
       }} />
       <input type="time" css={globalStyle.inputDateOrTime} value={time} onChange={(event) => {
         setTime(event.target.value);
       }} />
-    </>
+    </div>
   );
 };
