a1fe986728b054bae524e87821518c085a6d1921
Kohei Sato <kohei_sato@crosshead.co.jp>
2023-04-10 10:36:19 +0900

    feat: プラグイン設定 保存ボタンクリック時のfieldControlList state変更実装

--- src/settingScreen/components/PluginSetting.tsx
+++ src/settingScreen/components/PluginSetting.tsx
@@ -1,8 +1,9 @@
 import { css } from '@emotion/react';
-import { useRecoilValue } from 'recoil';
+import { useRecoilState } from 'recoil';
 import FieldControlList from './FieldControlList';
 import { fieldControlList } from '../store';
 import { PluginSetting } from '../../type';
+import { deepcp } from '../utils';
 
 const style = {
   container: css({
@@ -26,15 +27,62 @@ const style = {
 };
 
 export default (props: { show: boolean }) => {
-  const _fieldControlList = useRecoilValue(fieldControlList);
+  const [list, setList] = useRecoilState(fieldControlList);
+
+  const checkSetting = (): boolean => {
+    let isCorrect = true;
+
+    // fieldControlList check
+    const _list = deepcp(list).map(e => {
+      // targetField
+      if(e.targetField.value === null) {
+        isCorrect = false;
+        e.targetField.fieldError = true;
+        e.targetField.errorText = '設定してください';
+      } else {
+        e.targetField.fieldError = false;
+      }
+      // controlType 特になし
+      // config
+      return {
+        ...e,
+        config: e.config.map(f => {
+          // field 特になし
+          // op
+          if(f.field.value !== null && f.op.value === null) {
+            isCorrect = false;
+            f.op.fieldError = true;
+            f.op.errorText = '設定してください';
+          } else {
+            f.op.fieldError = false;
+          }
+          if(f.field.value !== null && f.value.value === null) {
+            isCorrect = false;
+            f.value.fieldError = true;
+            f.value.errorText = '設定してください';
+          } else {
+            f.value.fieldError = false;
+          }
+          return f;
+        })
+      };
+    });
+
+    if(!isCorrect) {
+      setList(_list);
+    }
+    
+    return isCorrect;
+  };
 
   const savePluginSettings = () => {
     // ValueCheck
+    if(!checkSetting()) return;
 
     const pluginSetting: PluginSetting = {
       systemSetting: { _: {} },
       customizeSetting: {
-        fieldControlList: _fieldControlList.map(e => ({
+        fieldControlList: list.map(e => ({
           targetField: e.targetField.value,
           controlType: e.controlType.value,
           config: e.config.map(f => ({
